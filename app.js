0d64973521c085f85dae8a19f88f6883d68d359a
santhosh <singumahanthisanthosh@gmail.com>
2018-10-29 18:23:13 +0530

    google-strategy-set up with env variables

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
 const authRoutes = require('./routes/auth-routes')
+
 //setting up view engine
 app.set('view engine','ejs');
 //setup routes
-
 app.use('/auth',authRoutes);
 //rendering home page
 app.get('/',(req,res)=>{
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const GoogleStategy = require('passport-google-oauth20');
 passport.use(new GoogleStategy({
 //options for the google to start
+    clientID : process.env.clientID,
+    clientSecret : process.env.clientSecret
 
 }),()=>{
      //passport call back function 
