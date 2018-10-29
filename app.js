7854176c1946861c9c458f109b1dbc7301089844
santhosh <singumahanthisanthosh@gmail.com>
2018-10-29 17:18:50 +0530

    styling added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-app.get('/',(req,res)=>{
-    res.send('Welcome to OAuth tutorial.');
+//setting up view engine
+app.set('view engine','ejs');
 
+//rendering home page
+app.get('/',(req,res)=>{
+res.render('home');
 });
+
+//Starting the server
 app.listen(3000,()=>{
     console.log('server started on port : 3000')
 })
