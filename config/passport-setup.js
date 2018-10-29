5abf425067ef2d35b936562fbba21afae3fd0642
santhosh <singumahanthisanthosh@gmail.com>
2018-10-29 19:40:49 +0530

    users created using google+ and added to mongodb database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStategy = require('passport-google-oauth20');
+const User = require('../models/user');  
 passport.use(new GoogleStategy({
 //options for the google to start
     clientID : process.env.clientID,
@@ -8,5 +9,12 @@ passport.use(new GoogleStategy({
 
 },(accessToken,refreshToken,profile,done)=>{
      //passport call back function 
+     console.log('passport function started!');
      console.log(profile);
+     new User({
+         username : profile.displayName, 
+         googleId : profile.id
+     }).save().then((newUser)=>{
+         console.log('new user created:'+newUser);
+     })
 }))
